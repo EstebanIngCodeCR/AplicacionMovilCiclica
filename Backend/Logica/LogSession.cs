cdb21e67a2463fb19f9ba9f9d7c6fedbceeff708
EstebanIngCodeCR <145230330+EstebanIngCodeCR@users.noreply.github.com>
2023-11-07 19:44:06 -0600

    cambios

--- Backend/Logica/LogSession.cs
+++ Backend/Logica/LogSession.cs
@@ -11,7 +11,7 @@ namespace Backend.Logica
     public class LogSession
     {
 
-        public Session obtenerSession(String session) {
+        public static Session obtenerSession(String session) {
             Session objetoSession = new Session();
             int? errorId = 0;
             int? idReturn = 0;//idusuario
@@ -23,6 +23,11 @@ namespace Backend.Logica
             {
                 //Agregar resto de campos 
                 objetoSession.SessionId = sessionLinq.SESSION_ID;
+                objetoSession.Session_User_Id = sessionLinq.SESSION_USER_ID;
+                objetoSession.SessionCod = sessionLinq.SESSION;
+                objetoSession.Session_Fecha_Inicio = (DateTime)sessionLinq.SESSION_FECHA_INICIO;
+                objetoSession.Session_Fecha_Final = (DateTime)sessionLinq.SESSION_FECHA_FINAL;
+                objetoSession.Session_Estado = (bool)sessionLinq.SESSION_ESTADO;
             }
             else
             {
@@ -31,22 +36,23 @@ namespace Backend.Logica
             return objetoSession;
         }
 
-        public Boolean ingresarSession(int idUsuario)
+        public static string ingresarSession(int idUsuario)
         {
             int? errorId = 0;
             int? idReturn = 0;//idusuario
             string errorDescripcion = "";
 
             conexionlinqDataContext miLinq = new conexionlinqDataContext();
+            string session = Guid.NewGuid().ToString();
             miLinq.sp_IngresarSession(idUsuario, Guid.NewGuid().ToString() ,ref idReturn, ref errorId, ref errorDescripcion);
             if (errorId == 0 && idReturn != 0)
             {
                 //Agregar resto de campos 
-                return true;
+                return session;
             }
             else
             {
-                return false;
+                return null;
             }
         }
     }
--- Backend/Logica/LogUsuario.cs
+++ Backend/Logica/LogUsuario.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
 
 namespace Backend.Logica
 {
@@ -110,15 +111,20 @@ namespace Backend.Logica
 
                     conexionlinqDataContext miLinq = new conexionlinqDataContext();
                     miLinq.SP_LoginUsuario(req.userLog.correo, req.userLog.contrasena, ref idReturn, ref errorId, ref errorDescripcion);
+
+                    string session = LogSession.ingresarSession((int)idReturn);
+
                     //Validar correo , contrasena y  **si la session se creo
-                    if (errorId == 0 && idReturn != 0)
+                    if (errorId == 0 && idReturn != 0 && session != null)
                     {
                         //Exitoso
                         res.resultado = true;
+                        res.session = session;
                     }
                     else
                     {
                         //Error en la base de datos
+                        res.session = null;
                         res.resultado = false;
                         res.errorMensaje = "Error de login";
                         res.errorCode = (int)EnumErrores.ErrorLogin;
@@ -128,6 +134,7 @@ namespace Backend.Logica
             }
             catch (Exception ex)
             {
+                res.session = null;
                 res.resultado = false;
                 res.errorMensaje = "Error interno";
                 res.errorCode = (int)EnumErrores.ErrorInterno;
