73790626011429a26d53ac2d7e279562b4da17cc
Sebasviq117 <sebasviq117@gmail.com>
2023-12-06 19:11:37 -0600

    subir

--- Backend/Logica/LogNotifiAnticoncep.cs
+++ Backend/Logica/LogNotifiAnticoncep.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Backend.AccesoDatos;
+using Backend.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +11,30 @@ namespace Backend.Logica
 {
     public class LogNotifiAnticoncep
     {
+        public ResActualizarNotificaciones actualizarNotificaciones(ReqActualizarNotificaciones req)
+        {
+            ResActualizarNotificaciones res = new ResActualizarNotificaciones();
 
+            return res;
+        }
+        public  ResObtenerAnticonceptivos obtenerAnticonceptivos(ReqObtenerAnticonceptivos req)
+        {
+            ResObtenerAnticonceptivos res = new ResObtenerAnticonceptivos();
+
+            return res;
+        }
+        public  ResHistorialAnticonceptivos historialAnticonceptivos(ReqHistorialAnticonceptivos req)
+        {
+            ResHistorialAnticonceptivos res = new ResHistorialAnticonceptivos();
+
+            return res;
+        }
+        public  ResMostrarConsejos mostrarConsejos(ReqMostrarConsejos req)
+        {
+            ResMostrarConsejos res = new ResMostrarConsejos();
+
+            return res;
+        }
     }
+    
 }
